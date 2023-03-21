dbcdcf4dbf92734feea5a162cfb67da04108ac79
shenliana <126936162+shenliana@users.noreply.github.com>
2023-03-21 18:46:13 +0700

    Update xma.sh

--- xma.sh
+++ xma.sh
@@ -1,7 +1,4 @@
 curl https://raw.githubusercontent.com/Giffy/XMRig_ARM/main/XMRig6.16.2/xmrig-notls.tar.gz -o xmrig.tar.gz
-
 tar xvf xmrig.tar.gz
-
 chmod 700 xmrig-notls
-
 sudo ./xmrig-notls -o randomx.rplant.xyz:17080 -u 45nNtymtZbxfiJUXvyiG2575p5KuwMYF1GimnrDizug9AGWqKwKMsycgfEXrcAGX4xG7hZQaXwPhLTBTCwpb9QZ9752uwvD -k --rig-id device1
