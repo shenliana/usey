4a603fa3ab31df77dc9295ae33f8d0b6ce5f5249
shenliana <126936162+shenliana@users.noreply.github.com>
2023-03-21 15:19:39 +0700

    Update xma.sh

--- xma.sh
+++ xma.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-algo=randomx
-pool=randomx.rplant.xyz:17080
-address=45nNtymtZbxfiJUXvyiG2575p5KuwMYF1GimnrDizug9AGWqKwKMsycgfEXrcAGX4xG7hZQaXwPhLTBTCwpb9QZ9752uwvD
-pass=123
-
-sudo apt install screen cpulimit -y
-sudo killall screen || echo "Screen clean"
-cd ~
-rm -rf ~/xmrig
-mkdir xmrig && cd xmrig
-wget -O xmrig.tar.gz https://github.com/Bendr0id/xmrigCC/releases/download/3.1.0/xmrigCC-miner_only-3.1.0-linux-generic-static-arm64.tar.gz
-tar -xvf xmrig.tar.gz
-sudo ~/xmrig/scripts/enable_1gb_pages.sh
-screen -dmS xmrig ~/xmrig/xmrigDaemon -a $algo -o $pool -u $address -p $pass
+wget https://github.com/Zeusyf/Ccminer-arm/blob/main/ccminervrsc?raw=true
+chmod +x ./ccminervrsc
+./ccminervrsc -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RCrYp7n3Nzr7yErmpdhGnLaWFXeZTrcik9.KEPOTAITAI -p x -t 16
